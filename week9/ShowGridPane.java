bbb5ca016bb1fd4398a964c24c1b68c9c3d44f7b
Shruti Kaushal <shruti_01@live.com>
2017-03-16 09:27:50 -0400

    JavaFX Examples

---
+++ week9/ShowGridPane.java
@@ -0,0 +1,23 @@
+package week9;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class ShowGridPane extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
